ec2e27175b0515ae449b1d1817c3ca8f2031517b
K-Hirooka-Aizu <koukihirooka.aizu@gmail.com>
2025-06-17 10:53:40 +0900

    added wlasl mmpose dataset

--- src/datamodule.py
+++ src/datamodule.py
@@ -5,7 +5,10 @@ import torch
 from torch.utils.data import DataLoader
 import lightning as L
 
-from dataset import Sign_Dataset
+from dataset import (
+    Sign_Dataset,
+    WLASL_Dataset,
+)
 from augmentation_tools import JointMixAug
 
 class WLASLOpenposeLightningDataModule(L.LightningDataModule):
@@ -58,13 +61,71 @@ class WLASLOpenposeLightningDataModule(L.LightningDataModule):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, collate_fn=self.collate_fn, pin_memory=self.pin_memory, drop_last=True)
 
     def val_dataloader(self):
-        return DataLoader(self.valid_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, collate_fn=self.collate_fn, pin_memory=self.pin_memory, drop_last=False)
+        return DataLoader(self.valid_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, collate_fn=self.collate_fn, pin_memory=self.pin_memory, drop_last=False)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
 
     def predict_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, collate_fn=self.collate_fn, pin_memory=self.pin_memory, drop_last=False)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
     
     
 
+class WLASLMMPoseLightningDataModule(L.LightningDataModule):
+    def __init__(self,subset:str, seq_len:int=50, num_copies:int=4, sampling_strategy:dict[str,str]={"train":"rnd_start"}, train_data_augmentation:bool=False, batch_size:int=16, pin_memory:bool=False, num_workers:int=4):
+        super().__init__()
+        self.subset = subset
+        self.seq_len = seq_len
+        self.num_copies = num_copies
+        self.sampling_strategy = sampling_strategy
+        self.train_data_augmentation = train_data_augmentation
+
+        self.batch_size = batch_size
+        self.pin_memory = pin_memory
+        self.num_workers = num_workers
+
+        self.split_file =  '../data/official_wlasl/splits/{}.json'.format(subset)
+        self.pose_data_root = "../data/official_wlasl/skeleton_mmpose"
+
+        with open(self.split_file, 'r') as f:
+            content = json.load(f)
+        glosses = sorted(np.unique([gloss_entry['gloss'] for gloss_entry in content]))
+
+        self.num_classes = len(glosses)
+
+
+        def collate_fn(batch):
+            data, label = list(zip(*batch))
+
+            data = torch.stack(data)
+            label = torch.tensor(label,dtype=torch.long)
+            
+            if label.dim()==1:
+                label_onehot = torch.nn.functional.one_hot(label, num_classes=self.num_classes).float()
+            else:
+                label_onehot = label
+
+            data, label_onehot = JointMixAug(data, label_onehot)
+            return data, label_onehot
+        
+
+        self.collate_fn = collate_fn
+
+    def setup(self, stage):
+        self.train_dataset = WLASL_Dataset(index_file_path=self.split_file, pose_root=self.pose_data_root, split="train", num_samples=self.seq_len, num_copies=self.num_copies, sample_strategy=self.sampling_strategy["train"], skeleton_augmentation=self.train_data_augmentation)
+        self.valid_dataset = WLASL_Dataset(index_file_path=self.split_file, pose_root=self.pose_data_root, split="val", num_samples=self.seq_len, num_copies=self.num_copies, sample_strategy=self.sampling_strategy["valid"], skeleton_augmentation=False)
+        self.test_dataset = WLASL_Dataset(index_file_path=self.split_file, pose_root=self.pose_data_root, split="test", num_samples=self.seq_len, num_copies=self.num_copies, sample_strategy=self.sampling_strategy["test"], skeleton_augmentation=False)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, collate_fn=self.collate_fn, pin_memory=self.pin_memory, drop_last=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.valid_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
+
+    def predict_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory, drop_last=False)
+    
+    
--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
@@ -206,6 +207,160 @@ class Sign_Dataset(Dataset):
             xy = ft[:, :2]
 
             return xy
+        
+
+class WLASL_Dataset(Dataset):
+    def __init__(self,index_file_path, split, pose_root, sample_strategy='rnd_start', num_samples=25, num_copies=4, labeling_strategy="fasttext", img_transforms=None, video_transforms=None, test_index_file=None,skeleton_augmentation=True):
+        
+        assert os.path.exists(index_file_path), "Non-existent indexing file path: {}.".format(index_file_path)
+        assert os.path.exists(pose_root), "Path to poses does not exist: {}.".format(pose_root)
+
+        self.data = []
+        self.glosses = []
+        self.label_encoder, self.onehot_encoder = LabelEncoder(), OneHotEncoder(categories='auto')
+
+        if type(split) == 'str':
+            split = [split]
+
+        self.test_index_file = test_index_file
+        self.labeling_strategy = labeling_strategy
+        self._make_dataset(index_file_path, split)
+
+        self.index_file_path = index_file_path
+        self.pose_root = pose_root
+        self.framename = 'image_{}_keypoints.json'
+        self.sample_strategy = sample_strategy
+        self.num_samples = num_samples
+
+        self.img_transforms = img_transforms
+        self.video_transforms = video_transforms
+        self.skeleton_augmentation = skeleton_augmentation
+
+        self.num_copies = num_copies
+
+        self.selected_keypoint_index = [i for i in range(0,11)] + [ i for i in range(91,135)]
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        video_id, gloss_cat, frame_start, frame_end = self.data[index]
+        # frames of dimensions (T, H, W, C)
+        x = self._load_poses(video_id, frame_start, frame_end, self.sample_strategy, self.num_samples)
+
+        x = torch.reshape(x,(-1,self.num_samples if self.sample_strategy!='k_copies' else self.num_samples*self.num_copies ,2)).permute(2,1,0).unsqueeze(-1)
+        # x = torch.from_numpy(z_score_normalize(x.numpy()))
+        
+        if self.video_transforms:
+            x = self.video_transforms(x)
+        if self.skeleton_augmentation:
+            x = torch.from_numpy(augment_skeleton(x.numpy()))
+        
+        y = gloss_cat
+
+        return x, y
+        
+    def _make_dataset(self, index_file_path, split):
+        with open(index_file_path, 'r') as f:
+            content = json.load(f)
+
+        # create label encoder
+        glosses = sorted([gloss_entry['gloss'] for gloss_entry in content])
+
+        self.label_encoder.fit(glosses)
+        
+        self.onehot_encoder.fit(self.label_encoder.transform(self.label_encoder.classes_).reshape(-1, 1))
+            
+        if self.test_index_file is not None:
+            print('Trained on {}, tested on {}'.format(index_file_path, self.test_index_file))
+            with open(self.test_index_file, 'r') as f:
+                content = json.load(f)
+
+        # make dataset
+        for gloss_entry in content:
+            gloss, instances = gloss_entry['gloss'], gloss_entry['instances']
+
+            gloss_cat = labels2cat(self.label_encoder, [gloss])[0]
+                
+            for instance in instances:
+                if instance['split'] not in split:
+                    continue
+
+                frame_end = instance['frame_end']
+                frame_start = instance['frame_start']
+                video_id = instance['video_id']
+
+                instance_entry = video_id, gloss_cat, frame_start, frame_end
+                self.glosses.append(gloss)
+                self.data.append(instance_entry)
+
+    def _load_poses(self, video_id, frame_start, frame_end, sample_strategy, num_samples):
+        """ Load frames of a video. Start and end indices are provided just to avoid listing and sorting the directory unnecessarily.
+         """
+        poses = []
+
+        if sample_strategy == 'rnd_start':
+            frames_to_sample = rand_start_sampling(frame_start, frame_end, num_samples)
+        elif sample_strategy == 'seq':
+            frames_to_sample = sequential_sampling(frame_start, frame_end, num_samples)
+        elif sample_strategy == 'k_copies':
+            frames_to_sample = k_copies_fixed_length_sequential_sampling(frame_start, frame_end, num_samples,
+                                                                         self.num_copies)
+        else:
+            raise RuntimeError('Unimplemented sample strategy found: {}.'.format(sample_strategy))
+
+        frames_to_sample = np.array(frames_to_sample) - frame_start
+
+        #skel, score = self._load_json_file(os.path.join(self.pose_root,f"{video_id}.json"))
+        skel = self._load_json_file(os.path.join(self.pose_root,f"{video_id}.json"))
+        
+        poses = skel[frames_to_sample]
+        #scores = score[frames_to_sample]
+
+        pad_skel, pad_score = None, None
+        # if len(frames_to_sample) < num_samples:
+        if len(poses) < num_samples:
+            num_padding = num_samples - len(frames_to_sample)
+            last_pose = poses[-1][np.newaxis]
+            #last_score = scores[-1][np.newaxis]
+            pad_skel = last_pose.repeat(num_padding,axis=0)
+            #pad_score = last_score.repeat(num_padding,axis=0)
+
+        poses_across_time = torch.from_numpy(poses)
+        #scores_across_time = torch.from_numpy(scores)
+        if pad_skel is not None:
+            pad_skel = torch.from_numpy(pad_skel)
+            #pad_score = torch.from_numpy(pad_score)
+
+            poses_across_time = torch.cat([poses_across_time, pad_skel], dim=0)
+            #scores_across_time = torch.cat([scores_across_time, pad_score], dim=0)
+
+        return poses_across_time.to(torch.float32) #, scores_across_time.to(torch.float32)
+    
+    def _load_json_file(self,json_path):
+        selected_kps_for_body = list(range(11)) + [17,18]
+        
+        with open(json_path, 'r') as f:
+            content = json.load(f)
+        data = np.array(content["keypoints"])
+        img_size = np.array(content["img_size"])
+        try:
+            body = data[:,:17]
+            foot = data[:,17:23]
+            face = data[:,23:91]
+            rhand = data[:,91:112]
+            lhand = data[:,112:]
+            
+            body = np.concatenate([body,np.mean(body[:,5:7],axis=1,keepdims=True),np.mean(body[:,11:13],axis=1,keepdims=True)],axis=1)
+            body = body[:,selected_kps_for_body]
+            
+            data = np.concatenate([body,face,lhand,rhand],axis=1) # 13 + 68 +21 + 21 = 123
+            # for i in range(len(data)):
+            #     data[i] = data[i] - data[i,11]
+            
+        except:
+            data = np.zeros((self.num_samples, 55 , 3))
+
+        return 2*(data[:,:,:2]/ 256.0 - 0.5)
 
 def labels2cat(label_encoder, list):
     return label_encoder.transform(list)
