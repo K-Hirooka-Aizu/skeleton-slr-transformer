3239a03a4243b19412a5121ad4d6970ff942d69b
K-Hirooka-Aizu <koukihirooka.aizu@gmail.com>
2025-06-24 07:47:45 +0900

    added gcn models

--- src/models/model.py
+++ src/models/model.py
@@ -6,6 +6,10 @@ from .transformers import (
     SpatialTemporalTransformer,
     SpatialTemporalTransformerWithClassToken,
 )
+from .gcn import(
+    STGCN,
+    CTRGCN
+)
 
 def build_model(cfg:DictConfig, **kwargs):
     model_name = cfg.model.model_name
@@ -13,5 +17,9 @@ def build_model(cfg:DictConfig, **kwargs):
         return SpatialTemporalTransformer(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
     elif model_name == "SpatialTemporalTransformerWithClassToken":
         return SpatialTemporalTransformerWithClassToken(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+    elif model_name == "stgcn":
+        return STGCN(in_channels=cfg.data.in_channels, num_class=cfg.data.num_classes, graph_args=cfg.model.graph_args, edge_importance_weighting=cfg.model.edge_importance_weighting)
+    elif model_name == "ctrgcn":
+        return CTRGCN(num_class=cfg.data.num_classes, num_point=cfg.data.n_joints, num_person=1, graph_args=cfg.model.graph_args, in_channels=cfg.data.in_channels, drop_out=cfg.model.drop_out, adaptive=cfg.model.adaptive)
     else:
         raise RuntimeError(f"Model_name [{model_name}] is not implemented.")
