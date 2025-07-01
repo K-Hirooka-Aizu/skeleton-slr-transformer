ab84566071ccc79e013ac6707b05a68319d5a78f
K Hirooka <koukihirooka.aizu@gmail.com>
2025-07-01 11:50:24 +0900

    implemented the prenorm transformer

--- src/models/model.py
+++ src/models/model.py
@@ -5,6 +5,8 @@ from omegaconf import DictConfig
 from .transformers import (
     SpatialTemporalTransformer,
     SpatialTemporalTransformerWithClassToken,
+    PreNormSpatialTemporalTransformer,
+    PreNormSpatialTemporalTransformerWithClassToken,
 )
 from .gcn import(
     STGCN,
@@ -17,6 +19,12 @@ def build_model(cfg:DictConfig, **kwargs):
         return SpatialTemporalTransformer(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
     elif model_name == "SpatialTemporalTransformerWithClassToken":
         return SpatialTemporalTransformerWithClassToken(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+
+    elif model_name == "PreNormSpatialTemporalTransformer":
+        return PreNormSpatialTemporalTransformer(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+    elif model_name == "PreNormSpatialTemporalTransformerWithClassToken":
+        return PreNormSpatialTemporalTransformerWithClassToken(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+
     elif model_name == "stgcn":
         return STGCN(in_channels=cfg.data.in_channels, num_class=cfg.data.num_classes, graph_args=cfg.model.graph_args, edge_importance_weighting=cfg.model.edge_importance_weighting)
     elif model_name == "ctrgcn":
--- src/models/transformers/__init__.py
+++ src/models/transformers/__init__.py
@@ -2,3 +2,7 @@ from .spatial_temporal_transformer import (
     SpatialTemporalTransformer,
     SpatialTemporalTransformerWithClassToken,
 )
+from .prenorm_transformer import (
+    PreNormSpatialTemporalTransformer,
+    PreNormSpatialTemporalTransformerWithClassToken,
+)
