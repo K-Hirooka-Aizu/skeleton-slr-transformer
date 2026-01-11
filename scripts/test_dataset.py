8996d4ba97b4d66c316b97ed23055630c343d724
K-Hirooka-Aizu <koukihirooka.aizu@gmail.com>
2026-01-12 00:46:27 +0900

    test: video dataloader testing script

--- scripts/test_dataset.py
+++ scripts/test_dataset.py
@@ -1,29 +1,30 @@
 from tqdm import tqdm
-from sstan.video_dataset import WLASLVideoDataset
-from sstan.dataset import Sign_Dataset
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-def main():
-    dataset = WLASLVideoDataset(
-        split_file_path="/home/hirooka/transformer-based-sign-language-recognition/data/official_wlasl/splits/asl100.json",
-        split="train",
-        video_dir_path="/home/hirooka/transformer-based-sign-language-recognition/data/official_wlasl/video",
-        seq_len=64,
-        sampling_strategy="rnd_start",
-        transforms=None
-    )
-    print(WLASLVideoDataset.gloss2index.keys())
+from sstan.datamodule import build_lightning_data_module
 
-    # subset = "asl100"
-    # dataset = Sign_Dataset(
-    #     index_file_path='./data/official_wlasl/splits/{}.json'.format(subset), 
-    #     pose_root="./data/official_wlasl/pose_per_individual_videos",
-    #     split="train",
-    #     num_samples=64, num_copies=4, sample_strategy="rnd_start", skeleton_augmentation=None)
-    
-    print(len(dataset))
-    for i, (data, label) in enumerate(tqdm(dataset)):
-        print(f"{type(data)}: {data.shape}")
-        print(f"{type(label)}: {label.shape}")
+@hydra.main(version_base=None, config_path="conf", config_name="default")
+def main(cfg:DictConfig):
+    datamodule = build_lightning_data_module(cfg)
+    datamodule.setup(None)
+
+    train_dataloader = datamodule.train_dataloader()
+    for i, ((video,mask), label) in enumerate(tqdm(train_dataloader)):
+        if i % 1000 == 0:
+            print(f"{video.shape} {mask.shape}, {label.shape}")
+        
+        pass
+    valid_dataloader = datamodule.val_dataloader()
+    for i, ((video,mask), label) in enumerate(tqdm(valid_dataloader)):
+        if i % 1000 == 0:
+            print(f"{video.shape} {mask.shape}, {label.shape}")
+        
+    test_dataloader = datamodule.test_dataloader()
+    for ((video,mask), label) in tqdm(test_dataloader):
+        pass
+
+
 
 if __name__=="__main__":
     main()
