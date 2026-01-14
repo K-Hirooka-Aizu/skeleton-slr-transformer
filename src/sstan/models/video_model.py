e8bb3c8ef07e1d04e418bfe57eaa7983e7fc6e74
K-Hirooka-Aizu <koukihirooka.aizu@gmail.com>
2026-01-14 12:34:09 +0900

    feat: r3d_18 from torchvision is available now

--- src/sstan/models/video_model.py
+++ src/sstan/models/video_model.py
@@ -1,6 +1,6 @@
 from omegaconf import DictConfig
 import torch.nn as nn
-from torchvision.models.video import s3d
+from torchvision.models.video import s3d, mvit_v2_s, r3d_18
 
 from .cnn.pytorch_i3d import InceptionI3d
 
@@ -18,5 +18,15 @@ def build_model(cfg:DictConfig, **kwargs):
         else:
             s3d_model = s3d(num_classes=cfg.data.num_classes)
         return s3d_model
+    
+    elif model_name == "r3d_18":
+        if cfg.model.model_args.pretrained:
+            r3d_model = r3d_18(weights='KINETICS400_V1')
+            _in_features = r3d_model.fc.in_features
+            r3d_model.fc = nn.Linear(_in_features, cfg.data.num_classes)
+        else:
+            r3d_model = r3d_18(num_classes=cfg.data.num_classes)
+        return r3d_model
+    
     else:
         raise RuntimeError(f"Model_name [{model_name}] is not implemented.")
