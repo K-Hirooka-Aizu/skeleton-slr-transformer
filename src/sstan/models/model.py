4e52523e35dc228bb24658f235d4d8227e771000
K-Hirooka-Aizu <koukihirooka.aizu@gmail.com>
2026-02-16 15:07:10 +0900

    feat: make normalization layer configurable between BatchNorm and LayerNorm

--- src/sstan/models/model.py
+++ src/sstan/models/model.py
@@ -15,9 +15,24 @@ from .gcn import(
 def build_model(cfg:DictConfig, **kwargs):
     model_name = cfg.model.model_name
     if model_name == "SpatialTemporalTransformer":
-        return SpatialTemporalTransformer(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+        return SpatialTemporalTransformer(
+            in_channels=cfg.data.in_channels, 
+            num_classes=cfg.data.num_classes, 
+            seq_len=cfg.data.seq_len, 
+            n_joints=cfg.data.n_joints, 
+            **cfg.model.model_args, 
+            **kwargs
+            )
+    
     elif model_name == "SpatialTemporalTransformerWithClassToken":
-        return SpatialTemporalTransformerWithClassToken(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
+        return SpatialTemporalTransformerWithClassToken(
+            in_channels=cfg.data.in_channels, 
+            num_classes=cfg.data.num_classes, 
+            seq_len=cfg.data.seq_len, 
+            n_joints=cfg.data.n_joints, 
+            **cfg.model.model_args, 
+            **kwargs
+            )
 
     elif model_name == "PreNormSpatialTemporalTransformer":
         return PreNormSpatialTemporalTransformer(in_channels=cfg.data.in_channels, num_classes=cfg.data.num_classes, seq_len=cfg.data.seq_len, n_joints=cfg.data.n_joints, **cfg.model.model_args, **kwargs)
